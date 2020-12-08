b63b9ce896cf43aed642f5cc340dd61726e4b1f0
Debjit Mukherjee <72387674+Debjit337@users.noreply.github.com>
2020-12-09 03:18:47 +0530

    student details work

---
+++ students_related_work.py
@@ -0,0 +1,11 @@
+def register_new_student(school_name, class_name):
+    print('\nregister_new_student_detail(class_name): ', class_name)
+
+def check_student_detail(class_name):
+    print('\ncheck_student_detail(class_name): ', class_name)
+
+def update_student_detail(class_name):
+    print('\nupdate_student_detail(class_name): ', class_name)
+
+def delete_student(class_name):
+    print('\ndelete_student(class_name): ', class_name)
